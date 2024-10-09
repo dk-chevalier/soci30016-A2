aa41c3b36794c7f41990e73aba9a93d73a803fc2
dk-chevalier <contact@chevalierrecords.com>
2024-10-09 18:16:19 +1100

    fixed close button on map image

--- SOCI30016-A2/src/pages/Map.jsx
+++ SOCI30016-A2/src/pages/Map.jsx
@@ -10,8 +10,7 @@ function Map() {
 
   return (
     <>
-      <div className="absolute w-[80vw] h-[90vh] z-50 flex justify-center items-start overflow-auto bg-slate-800 rounded-lg">
-        <img src={image} className="border border-slate-800"></img>
+      <div className="absolute w-[80vw] h-[90vh] z-50  bg-slate-800 rounded-lg">
         <NavLink
           to="../"
           className="absolute top-4 right-4 text-lg text-center w-6 h-6 hover:scale-110 active:scale-100 duration-300 text-white"
@@ -20,6 +19,9 @@ function Map() {
         >
           x
         </NavLink>
+        <div className="w-full h-full flex justify-center items-start overflow-auto">
+          <img src={image} className="border border-slate-800"></img>
+        </div>
       </div>
     </>
   );
